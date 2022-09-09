c445d207f315cfa1f48c103dd4e669926e955dde
AveryZiQiZhou <zzhou@bayviewglen.ca>
2022-09-09 11:07:42 -0400

    Initial Commit

---
+++ Unit One-Introduction to Computer Science/src/day2/ExampleOne.java
@@ -0,0 +1,12 @@
+package day2;
+
+/**
+ * ExampleOne
+ */
+public class ExampleOne {
+
+    public static void main(String[] args) {
+        System.out.println("Welcome");
+        
+    }
+}
---
+++ Unit One-Introduction to Computer Science/src/day2/ExampleTwo.java
@@ -0,0 +1,12 @@
+package day2;
+
+/**
+ * ExampleTwo
+ */
+public class ExampleTwo {
+
+    public static void main(String[] args) {
+    
+
+    }
+}
