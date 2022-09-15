17b530372fa169e69ab253227b9a513a8c09b15e
AveryZiQiZhou <zzhou@bayviewglen.ca>
2022-09-15 10:47:27 -0400

    day3Example34

--- Unit One-Introduction to Computer Science/src/day3/ExampleThree.java
+++ Unit One-Introduction to Computer Science/src/day3/ExampleThree.java
@@ -6,5 +6,20 @@ public class ExampleThree {
     System.out.println(17%5);   //modules (reminder)
     System.out.println((9+10)%12);
     System.out.println(5+8/3*2);
+
+    int number=123456;
+    System.out.println(number/10);  //12345
+    System.out.println(number%10);  //6
+
+    int digit1=number/100000;
+    int digit2=number/10000%10; 
+    int digit3=number/1000%10;
+    int digit4=number/100%10;
+    int digit5=number/10%10;
+    int digit6=number%10;
+
+    int sum=digit1+digit2+digit3+digit4+digit5+digit6;
+    System.out.println(sum);
+
   }  
 }
